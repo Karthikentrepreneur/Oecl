27a1872a67cb5b5b34076395e820d41cc282d3e9
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-05 01:09:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Services from "./pages/Services";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import aboutus from "./pages/aboutus";
+import AboutUs from "./pages/aboutus";
 import Dashboard from "./pages/Dashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import NotFound from "./pages/NotFound";
