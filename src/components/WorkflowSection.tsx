e1bdfa6dea799fab88f8f01aaa2f210b62fc44ee
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-05 00:17:30 +0530

    Update WorkflowSection.tsx

--- src/components/WorkflowSection.tsx
+++ src/components/WorkflowSection.tsx
@@ -1,5 +1,4 @@
 import { ClipboardCheck, Package, Truck, CheckCircle } from "lucide-react";
-import ScrollAnimation from "./ScrollAnimation";
 const steps = [{
   id: 1,
   title: "Order Processing",
