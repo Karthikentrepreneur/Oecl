bd7b4227db563f6eddff4ef1b168e750006e18f4
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-10 15:00:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -77,18 +77,22 @@ const App = () => {
                 <Route path="/india/services" element={<IndiaServices />} />
                 <Route path="/india/about-us" element={<AboutUs />} />
                 <Route path="/india/contact" element={<Contact />} />
+                <Route path="/india/blogs" element={<blogs />} />
                 
                 <Route path="/indonesia/home" element={<IndonesiaHome />} />
                 <Route path="/indonesia/about-us" element={<AboutUs />} />
                 <Route path="/indonesia/contact" element={<Contact />} />
+                <Route path="/indonesia/blogs" element={<blogs />} />
                 
                 <Route path="/malaysia/home" element={<MalaysiaHome />} />
                 <Route path="/malaysia/about-us" element={<AboutUs />} />
                 <Route path="/malaysia/contact" element={<Contact />} />
+                <Route path="/malaysia/blogs" element={<blogs />} />
                 
                 <Route path="/thailand/home" element={<ThailandHome />} />
                 <Route path="/thailand/about-us" element={<AboutUs />} />
                 <Route path="/thailand/contact" element={<Contact />} />
+                <Route path="/thailand/blogs" element={<blogs />} />
                 
                 <Route path="/privacy-policy" element={<PrivacyPolicy />} />
                 <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
