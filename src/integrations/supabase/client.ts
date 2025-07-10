7591c31041431db02c297685c823461c5919c71e
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-11 02:19:28 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://qkszvkszjvacqqwhyqab.supabase.co";
+const SUPABASE_URL = "https://lbimikyjhyewlssnaegl.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiaW1pa3lqaHlld2xzc25hZWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNTA3NTYsImV4cCI6MjA2NzcyNjc1Nn0.CFRWvEcR9P5YNqHwI1wJ7qsCTuGTO_MwRAq5nhRr3xo"; // your anon key
 
 export const supabase = createClient<Database>(
