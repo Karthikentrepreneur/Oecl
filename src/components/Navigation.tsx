1b48df534d00a826f9289b75d20bf9f208a458fe
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-15 01:56:10 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -46,11 +46,10 @@ const Navigation = () => {
           {/* Country Selector & Quote Button */}
           <div className="hidden md:flex items-center gap-4">
             <CountrySelector />
-            <Link to="/Contact#contact-form">
-              <Button className="bg-kargon-red hover:bg-kargon-red/90 text-white rounded-md">
-                GET QUOTE
-              </Button>
-            </Link>
+            <Link to="/contact#contact-form">
+      <Button className="bg-kargon-red hover:bg-kargon-red/90 text-white rounded-md">
+        GET QUOTE
+      </Button>
           </div>
 
           {/* Mobile Menu Button */}
