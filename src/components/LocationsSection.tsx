3cec8594900dbcfebc0de02d6ea7a2f26e1a38f0
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-17 23:10:17 +0530

    Update LocationsSection.tsx

--- src/components/LocationsSection.tsx
+++ src/components/LocationsSection.tsx
@@ -19,51 +19,81 @@ const allLocations: LocationsData = {
   India: {
     Chennai: {
       map: "https://www.google.com/maps/d/embed?mid=1xTmhWpagroJz2YqtOTxHBEAZP8Xw_cA&ehbc=2E312F&noprof=1",
-      address: "Roma Building, Door No. 10, 3rd Floor, G.S.T. Road, Alandur, Chennai-600 016",
-      phone: "044 4689 4646",
+      address: "Chennai Office Address Here",
+      phone: "+91 9123523496",
     },
     Chennai2: {
       map: "https://www.google.com/maps/d/embed?mid=1prpWPmOJpIfxQg4LmwnRIyNE8pXue20&ehbc=2E312F&noprof=1",
-      address: "Survey No.209/6A(Part)...",
-      phone: "+91 9994355523",
+      address: "Chennai 2 Office Address Here",
+      phone: "+91 9876543210",
+    },
+    Delhi: {
+      map: "https://www.google.com/maps/d/embed?mid=1prpWPmOJpIfxQg4LmwnRIyNE8pXue20&ehbc=2E312F&noprof=1",
+      address: "Delhi Office Address Here",
+      phone: "+91 9999022030",
+    },
+    Kolkata: {
+      map: "https://www.google.com/maps/d/embed?mid=1rM6AXz8EHQ7xg4aldjPH2AJuY5NtapY&ehbc=2E312F&noprof=1",
+      address: "Kolkata Office Address Here",
+      phone: "+91 6290921534",
+    },
+    Bengaluru: {
+      map: "https://www.google.com/maps/d/embed?mid=1VfZTXJHg3ekHgdcyt3lf9cbsEnf11oA&ehbc=2E312F&noprof=1",
+      address: "Bengaluru Office Address Here",
+      phone: "+91 9986949743",
+    },
+    Cochin: {
+      map: "https://www.google.com/maps/d/embed?mid=1wFWiRd2ewEGxxGv2NBKTwJX7eClgjv0&ehbc=2E312F&noprof=1",
+      address: "Cochin Office Address Here",
+      phone: "+91 9012345678",
+    },
+    Hyderabad: {
+      map: "https://www.google.com/maps/d/embed?mid=1nfVu0PP4O3WJ9ZUqbJjpnKCblkY9rJI&ehbc=2E312F&noprof=1",
+      address: "Hyderabad Office Address Here",
+      phone: "+91 9123456789",
+    },
+    Mumbai: {
+      map: "https://www.google.com/maps/d/embed?mid=1ndB2LDzMO0nBfUDdGe2o7-_BpQxQRUQ&ehbc=2E312F&noprof=1",
+      address: "Mumbai Office Address Here",
+      phone: "+91 2245174102",
     },
   },
   Singapore: {
     Singapore: {
       map: "https://www.google.com/maps/d/embed?mid=1dMWuQ-5-EQL2a1gOFRIYFxVLUdfERNw&ehbc=2E312F&noprof=1",
-      address: "Blk 511 Kampong Bahru Road, #03-01 Keppel Distripark, Singapore 099447",
-      phone: "+65 6224 1338 / +65 6224 1336",
+      address: "Singapore Office Address Here",
+      phone: "+65 12345678",
     },
   },
   Malaysia: {
     PortKlang: {
       map: "https://www.google.com/maps/d/embed?mid=1wY9IR1V-C3m6rqDjbDVuARcHIn5DaX0&ehbc=2E312F&noprof=1",
-      address: "MTBBT 2, 3A-5, Jalan Batu Nilam 16...",
-      phone: "+603 - 3319 2778 / 74 / 75",
+      address: "Port Klang Office Address Here",
+      phone: "+60 123456789",
     },
     PasirGudang: {
       map: "https://www.google.com/maps/d/embed?mid=1Ef2sco0B8ZB3fShOADFD17mX6eEn22g&ehbc=2E312F&noprof=1",
-      address: "Unit 20-03A, Level 20 Menara Zurich...",
-      phone: "603-3319 2778 / 74 / 75, 79",
+      address: "Pasir Gudang Office Address Here",
+      phone: "+60 987654321",
     },
   },
   Thailand: {
     Bangkok: {
       map: "https://www.google.com/maps/d/embed?mid=1-KDMSa2eOSnchrGfGbRCMws3wdHa4tc&ehbc=2E312F&noprof=1",
-      address: "109 CCT Building, 3rd Floor...",
-      phone: "+662-634-3240, +662-634-3942",
+      address: "Bangkok Office Address Here",
+      phone: "+66 12345678",
     },
   },
   Indonesia: {
     Jakarta: {
       map: "https://www.google.com/maps/d/embed?mid=1wNAxoHYpLj-a8ekPGklU6BpnaBaz0Co&ehbc=2E312F&noprof=1",
-      address: "408, Lina Building, JL.HR Rasuna Said...",
-      phone: "+62 21 529 20292, 522 4887",
+      address: "Jakarta Office Address Here",
+      phone: "+62 812345678",
     },
     Surabaya: {
       map: "https://www.google.com/maps/d/embed?mid=1ecTq28A-CHVdwkhZB9M1HpLKe-xPKo0&ehbc=2E312F&noprof=1",
-      address: "Japfa Indoland Center, Japfa Tower 1...",
-      phone: "+62 21 529 20292, 522 4887",
+      address: "Surabaya Office Address Here",
+      phone: "+62 876543210",
     },
   },
 };
@@ -99,7 +129,7 @@ const LocationsSection: React.FC = () => {
     <div className="px-4 py-8 max-w-7xl mx-auto">
       <div className="mb-6">
         <h2 className="text-3xl font-bold mb-4 text-center">Our Office Locations</h2>
-        <div className="text-center text-xl font-semibold py-2 px-4 bg-blue-900 text-white rounded inline-block">
+        <div className="text-center text-xl font-semibold py-2 px-4 bg-red-600 text-white rounded inline-block">
           {selectedCountry}
         </div>
       </div>
@@ -132,7 +162,7 @@ const LocationsSection: React.FC = () => {
           </div>
 
           <div className="relative rounded-lg overflow-hidden h-[400px] shadow-lg">
-            <div className="absolute top-0 left-0 w-full text-center py-2 bg-red-800 font-semibold z-10">
+            <div className="absolute top-0 left-0 w-full text-white text-center py-2 bg-red-600 font-semibold z-10">
              {selectedLocation}
             </div>
             <iframe
