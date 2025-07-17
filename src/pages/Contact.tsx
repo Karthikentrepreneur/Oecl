9c5be37980224911e84a3eb81188a679baf7e6f2
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-17 18:15:58 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -152,12 +152,6 @@ const Contact = () => {
               </p>
             </motion.div>
 
-            <motion.div 
-              initial={{ opacity: 0 }} 
-              animate={{ opacity: 1 }} 
-              transition={{ duration: 0.5 }} 
-              className="flex flex-1 relative overflow-hidden"
-            >
 
               <section>
   <div>
