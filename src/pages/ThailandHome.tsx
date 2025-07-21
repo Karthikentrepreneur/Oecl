c7d090216de1e91b96be82929ed5b29c643e9b6d
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-21 11:16:09 +0530

    Update ThailandHome.tsx

--- src/pages/ThailandHome.tsx
+++ src/pages/ThailandHome.tsx
@@ -48,7 +48,6 @@ const ThailandHome = () => {
       <AboutSection />
       <ServicesSection />
       <WorkflowSection />
-      <TestimonialsSection />
       <GlobalPresence />
       <UpdatesSection />
       <ContactForm />
